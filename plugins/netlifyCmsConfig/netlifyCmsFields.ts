9f7a9c1bf9af406a4892f32a8a944cc9ad9d18b8
Daniel Gladman <d.gladman.anshincare@gmail.com>
2025-11-27 15:41:12 +0900

    change field labels

--- plugins/netlifyCmsConfig/netlifyCmsFields.ts
+++ plugins/netlifyCmsConfig/netlifyCmsFields.ts
@@ -8,14 +8,22 @@ export interface NetlifyCmsField {
   widget: string;
   required?: boolean;
   min?: number;
+  pattern?: string[];
+  hint?: string;
   fields?: NetlifyCmsField[]; // for object widget
 }
 
 // The base fields applied to each generated collection.
 export const baseFields: NetlifyCmsField[] = [
-  { name: 'id', label: 'Id', widget: 'string' },
-  { name: 'title', label: 'Title', widget: 'string' },
-  { name: 'subtitle', label: 'Subtitle', widget: 'string', required: false },
+  { 
+    name: 'id', 
+    label: 'id', 
+    widget: 'string', 
+    pattern: ['^[a-z0-9]+(?:-[a-z0-9]+)*$', '半角英小文字、数字、ハイフンのみ使用できます。先頭・末尾のハイフンや連続するハイフンは使用できません。'],
+    hint: 'IDには半角英小文字、数字、ハイフンのみを使用してください。先頭や末尾にハイフンを付けたり、連続してハイフンを使用しないでください。'
+  },
+  { name: 'title', label: 'タイトル', widget: 'string' },
+  { name: 'subtitle', label: 'サブタイトル', widget: 'string', required: false },
   // { name: 'sidebar_position', label: 'Sidebar Position', widget: 'number', required: false, min: 1},
   // {
   //   name: 'metadata',
@@ -27,7 +35,7 @@ export const baseFields: NetlifyCmsField[] = [
   //     { name: 'guideId', label: 'Guide ID', widget: 'string', required: false },
   //   ],
   // },
-  { name: 'body', label: 'Body', widget: 'markdown' },
+  { name: 'body', label: '本文', widget: 'markdown' },
 ];
 
 // If you need per-collection overrides in future, you can expose a helper:
