e33a049a01d8f8f8014977afb7efd0f7d6a75629
Daniel Gladman <d.gladman.anshincare@gmail.com>
2025-11-27 15:52:15 +0900

    add regex rules for fields subject to sanitization

--- plugins/netlifyCmsConfig/netlifyCmsFields.ts
+++ plugins/netlifyCmsConfig/netlifyCmsFields.ts
@@ -22,8 +22,19 @@ export const baseFields: NetlifyCmsField[] = [
     pattern: ['^[a-z0-9]+(?:-[a-z0-9]+)*$', '半角英小文字、数字、ハイフンのみ使用できます。先頭・末尾のハイフンや連続するハイフンは使用できません。'],
     hint: 'IDには半角英小文字、数字、ハイフンのみを使用してください。先頭や末尾にハイフンを付けたり、連続してハイフンを使用しないでください。'
   },
-  { name: 'title', label: 'タイトル', widget: 'string' },
-  { name: 'subtitle', label: 'サブタイトル', widget: 'string', required: false },
+  { 
+    name: 'title', 
+    label: 'タイトル', 
+    widget: 'string', 
+    pattern: ['^.{1,80}$', 'タイトルは1〜80文字で入力してください。']
+  },
+  { 
+    name: 'subtitle', 
+    label: 'サブタイトル', 
+    widget: 'string', 
+    required: false,
+    pattern: ['^.{0,140}$', 'サブタイトルは140文字以内で入力してください。']
+  },
   // { name: 'sidebar_position', label: 'Sidebar Position', widget: 'number', required: false, min: 1},
   // {
   //   name: 'metadata',
